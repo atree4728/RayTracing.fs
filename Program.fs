85f7be1b4a26a4de3b81869d4fd545c184a179d1
atree4728 <atree.public@gmail.com>
2024-09-10 23:00:24 +0900

    2.3: Adding a Progress Indicator

--- Program.fs
+++ Program.fs
@@ -9,9 +9,14 @@ let header =
 
 let scale d = d * 255.999 |> int
 
+let logger (remaining: int) =
+    eprintfn $"Scanlines remaining: {remaining}"
+
 let body =
     seq {
         for j in 0 .. imageHeight - 1 do
+            logger (imageHeight - j)
+
             for i in 0 .. imageWidth - 1 do
                 let r = float i / float (imageWidth - 1) |> scale
                 let g = float j / float (imageHeight - 1) |> scale
