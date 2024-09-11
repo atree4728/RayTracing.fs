ba553208a34ad496a149f4f67ccdf818d85f90a3
atree4728 <atree.public@gmail.com>
2024-09-11 13:42:04 +0900

    4.2: Sending Rays Into the Scene

--- Color.fs
+++ Color.fs
@@ -10,3 +10,13 @@ type Color =
         let g = this.g * 255.999 |> byte
         let b = this.b * 255.999 |> byte
         $"{r} {g} {b}"
+
+    static member inline (+)(c, d) =
+        { r = c.r + d.r
+          g = c.g + d.g
+          b = c.b + d.b }
+
+    static member inline (*)(t, color) =
+        { r = t * color.r
+          g = t * color.g
+          b = t * color.b }
--- Program.fs
+++ Program.fs
@@ -1,8 +1,42 @@
 ﻿open Color
 open Vector
+open Ray
 
-let imageWidth = 256
-let imageHeight = 256
+let rayColor (ray: Ray) =
+    let unitDirection = normalize ray.direction
+    let scaler = (unitDirection.y + 1.) / 2.
+    let white = { r = 1; g = 1; b = 1 }
+    let blue = { r = 0.5; g = 0.7; b = 1 }
+    (1. - scaler) * white + scaler * blue
+
+
+let imageWidth = 400
+
+let imageHeight =
+    let aspectRatio = 16. / 9.
+    float imageWidth / aspectRatio |> int |> max 1
+
+let aspectRatio = float imageWidth / float imageHeight
+
+let focalLength = 1.
+let viewportHeight = 2.
+let viewportWidth = viewportHeight * aspectRatio
+let cameraCenter = { x = 0; y = 0; z = 0 }
+
+let viewportU = { x = viewportWidth; y = 0; z = 0 }
+let viewportV = { x = 0; y = -viewportHeight; z = 0 }
+
+let pixelDeltaU = viewportU / float imageWidth
+let pixelDeltaV = viewportV / float imageHeight
+
+let viewportUpperLeft =
+    cameraCenter
+    - { x = 0; y = 0; z = focalLength }
+    - viewportU / 2.
+    - viewportV / 2.
+
+let pixel00Loc = viewportUpperLeft + (pixelDeltaU + pixelDeltaV) * 0.5
+
 
 let header =
     $"""P3
@@ -19,10 +53,14 @@ let body =
             logger (imageHeight - j)
 
             for i in 0 .. imageWidth - 1 do
-                let r = float i / float (imageWidth - 1)
-                let g = float j / float (imageHeight - 1)
-                let b = 0.
-                let color = { r = r; g = g; b = b }
+                let pixelCenter = pixel00Loc + pixelDeltaU * float i + pixelDeltaV * float j
+                let rayDirection = pixelCenter - cameraCenter
+
+                let ray =
+                    { origin = cameraCenter
+                      direction = rayDirection }
+
+                let color = rayColor ray
                 $"{color}\n"
     }
     |> Seq.reduce (+)
